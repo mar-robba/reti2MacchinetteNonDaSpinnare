6c23346fa9da2ae75dac84d3a1e006d2ca5e507f
Marco <marcorobba03@gmail.com>
2026-03-09 20:21:18 +0100

    aggiuta del readme del progetto

--- webapp/SmartFeederWebApp/obj/Debug/net10.0/SmartFeederWebApp.AssemblyInfo.cs
+++ webapp/SmartFeederWebApp/obj/Debug/net10.0/SmartFeederWebApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SmartFeederWebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+089cbc466d1646690824dc1931a6e53b59a7b42f")]
 [assembly: System.Reflection.AssemblyProductAttribute("SmartFeederWebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SmartFeederWebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
